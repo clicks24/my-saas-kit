e47d1d3307dea1c4ec30df2c97f9430011aad215
romanwbruce <71509815+romanwbruce@users.noreply.github.com>
2023-09-07 16:25:42 -0400

    improvement: add uploadthing url

--- src/env/server.ts
+++ src/env/server.ts
@@ -6,7 +6,8 @@ type serverSchema = {
     RESEND_SECRET: string,
     UPLOADTHING_SECRET: string,
     UPLOADTHING_APP_ID: string,
-    NEXTAUTH_URL: string
+    NEXTAUTH_URL: string,
+    UPLOADTHING_URL: string
 }
 
 export const serverEnv = {
@@ -16,5 +17,6 @@ export const serverEnv = {
     RESEND_SECRET: String(process.env.RESEND_SECRET),
     UPLOADTHING_SECRET: String(process.env.UPLOADTHING_SECRET),
     UPLOADTHING_APP_ID: String(process.env.UPLOADTHING_APP_ID),
-    NEXTAUTH_URL: String(process.env.NEXTAUTH_URL)
+    NEXTAUTH_URL: String(process.env.NEXTAUTH_URL),
+    UPLOADTHING_URL: String(process.env.UPLOADTHING_URL)
 } satisfies serverSchema
