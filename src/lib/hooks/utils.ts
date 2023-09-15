b29416a1ac3456acd5c64abd695e30f48dbf84c3
romanwbruce <71509815+romanwbruce@users.noreply.github.com>
2023-09-15 11:07:31 -0400

    Update utils.ts

--- src/lib/hooks/utils.ts
+++ src/lib/hooks/utils.ts
@@ -4,7 +4,6 @@ import { getSignedInUser } from "../auth/helper";
 import { getSubscriptions } from "../billing";
 
 export async function fetchSubscriptions() {
-
-    const session = await getSignedInUser();
-    return await getSubscriptions(session.user.stripe_customer_id);
-  }
+  const session = await getSignedInUser();
+  return await getSubscriptions(session.user.stripe_customer_id);
+}
