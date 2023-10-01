51f9e924cda3c70f432810ca024e40edcff426df
romanwbruce <71509815+romanwbruce@users.noreply.github.com>
2023-10-01 17:53:44 -0400

    fix: dark mode onboarding screen

--- src/app/onboarding/layout.tsx
+++ src/app/onboarding/layout.tsx
@@ -8,7 +8,7 @@ export default async function OnboardingLayout({
       <div className="grid place-items-center sm:min-h-screen sm:py-0 py-8">
         <div className="flex flex-col">
           <h2 className="sm:text-3xl font-medium text-lg text-center pb-8">👋 Welcome, {"let's get started"}</h2>
-          <div className="w-full bg-white sm:min-w-[400px] min-w-[300px] max-w-sm p-8 border dark:border-zinc-800 border-zinc-200 rounded-xl">
+          <div className="w-full light:bg-white sm:min-w-[400px] min-w-[300px] max-w-sm p-8 border dark:border-zinc-800 border-zinc-200 rounded-xl">
             {children}
           </div>
         </div>
